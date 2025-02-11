6c6c793d0b063055897920d524efb693fb263d25
Teerapong <teera04164@gmail.com>
2025-02-11 22:18:37 +0700

    add query key

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,8 @@
+export const QUERY_KEYS = {
+    PROFILE: 'profile',
+    COMMUNITIES: 'communities',
+    POSTS: 'posts',
+    POST: 'post',
+    COMMENTS: 'comments',
+    AUTH_USER: 'auth-user'
+}

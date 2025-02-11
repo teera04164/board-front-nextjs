31a986904c9b4e8ad38fb05b6ec338ddda2bf33d
Teerapong <teera04164@gmail.com>
2025-02-11 21:38:50 +0700

    add community api service

---
+++ src/services/community.service.ts
@@ -0,0 +1,20 @@
+
+import { Community } from '@/types/response/community.type';
+import { axiosInstance } from './axios';
+
+export const communityService = {
+  async getCommunities(): Promise<Community[]>  {
+    const response = await axiosInstance.get<Community[]>('/communities');
+    return response.data;
+  },
+
+  async getCommunity(id: string) {
+    const response = await axiosInstance.get<Community[]>(`/communities/${id}`);
+    return response.data;
+  },
+
+  async createCommunity(name: string) {
+    const response = await axiosInstance.post<Community>('/communities', { name });
+    return response.data;
+  },
+};

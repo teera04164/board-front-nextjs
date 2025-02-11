34f39b62471d98d93d74b32ab3244e50b11370e7
Teerapong <teera04164@gmail.com>
2025-02-11 22:06:40 +0700

    add notfound post

---
+++ src/components/posts/PostNotfound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostNotfound() {
+    return (
+        <div className="text-center mt-10">
+            <h1 className="text-lg font-semibold text-gray-300">No posts found</h1>
+        </div>
+    )
+}
+
+export default PostNotfound

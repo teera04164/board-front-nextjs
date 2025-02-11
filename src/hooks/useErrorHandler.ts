fda0bef4d0eb2c5ffba4a7d52c33943632a4ad9b
Teerapong <teera04164@gmail.com>
2025-02-11 22:05:02 +0700

    add use Error handler

---
+++ src/hooks/useErrorHandler.ts
@@ -0,0 +1,24 @@
+import { useCallback } from 'react';
+import { toast } from 'react-toastify';
+import { getErrorMessage } from '@/utils/error-handler';
+import { ERROR_CODES } from '@/constants/error';
+import { useRouter } from 'next/navigation';
+import { ROUTE_PATH } from '@/constants/route';
+
+export const useErrorHandler = () => {
+    const router = useRouter();
+
+    const handleError = useCallback((error: unknown, isRedirect?: boolean) => {
+        const errorResult = getErrorMessage(error);
+
+        toast.error(errorResult.message);
+
+        if (errorResult.code === ERROR_CODES.UNAUTHORIZED && isRedirect) {
+            router.push(ROUTE_PATH.LOGIN);
+        }
+
+        return errorResult;
+    }, []);
+
+    return { handleError };
+};

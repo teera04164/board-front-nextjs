63a1f4261e1312f9cbc32f25a9b3f820f12d20a7
Teerapong <teera04164@gmail.com>
2025-02-11 20:24:17 +0700

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,32 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        success: "var(--success)",
+        critical: "var(--critical)",
+        'green-100': '#D8E9E4',
+        'green-300': '#2B5F44',
+        'green-500': '#243831',
+        'gray-100': '#BBC2C0',
+        'gray-300': '#939494',
+        
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: '#49A569',
+          secondary: '#FF5722',
+          accent: '#FFC107',
+          neutral: '#1F2937',
+          'base-100': '#ffffff',
+          'green-300': '#2B5F44',
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 } satisfies Config;

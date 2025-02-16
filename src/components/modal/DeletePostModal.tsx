8252e7eae79434cc673bb649ddde92ce0e02bab8
Teerapong <teera04164@gmail.com>
2025-02-16 17:34:08 +0700

    fix size modal

--- src/components/modal/DeletePostModal.tsx
+++ src/components/modal/DeletePostModal.tsx
@@ -15,16 +15,18 @@ export const DeletePostModal: React.FC<IDeletePostModal> = ({ isOpen, onClose, o
             isOpen={isOpen}
             onClose={onClose}
             title={"Please confirm if you wish to delete the post"}
-            size="lg"
+            size="sm"
+            showCloseButton={false}
         >
-            <div className="space-y-3">
-                <div>
+            <div className="">
+                <div className="text-sm text-gray-500">
                     Are you sure you want to delete the post? Once deleted, it cannot be recovered.
                 </div>
                 <div className="flex flex-col md:flex-row md:justify-end gap-3 mt-8">
                     <Button
                         variant="outline"
                         onClick={onClose}
+                        
                     >
                         Cancel
                     </Button>

7c14ad7c4985c8ba768ec2aa4c1ab92844db56f3
Teerapong <teera04164@gmail.com>
2025-02-16 10:33:53 +0700

    change redirect to middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,4 @@
 "use client"
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { ROUTE_PATH } from "@/constants/route";
-
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push(ROUTE_PATH.BORD);
-  }, [router]);
-
   return null;
 }

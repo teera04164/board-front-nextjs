0a201b0c4a97f3fbf434056a89e848b02d1c0ced
Teerapong <teera04164@gmail.com>
2025-02-16 10:43:44 +0700

    add hooks menus

---
+++ src/hooks/useMenusItem.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { useCheckAuth } from "./useCheckAuth";
+import { menuItems } from "@/constants/menu";
+import { MenuItem } from "@/types/constant.type";
+
+export const useMenusItem = () => {
+    const [menus, setMenus] = useState<MenuItem[]>([]);
+    const { isAuthenticated } = useCheckAuth();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            setMenus(menuItems.filter((item) => !item?.isPivate));
+        } else {
+            setMenus(menuItems);
+        }
+    }, [isAuthenticated]);
+
+    return { menus };
+}

8b3c55ba0e435467268925c24d3e4e672cc6de6d
Teerapong <teera04164@gmail.com>
2025-02-16 18:31:13 +0700

    move navbar to layout

--- src/app/(bord)/layout.tsx
+++ src/app/(bord)/layout.tsx
@@ -1,10 +1,8 @@
 import BordLayout from '@/components/layout/BordLayout'
-import { Navbar } from '@/components/navbar/Navbar'
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <BordLayout>
-      <Navbar />
       {children}
     </BordLayout>
   )
--- src/components/layout/BordLayout.tsx
+++ src/components/layout/BordLayout.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { Sidebar } from '../sidebar/Sidebar'
+import { Navbar } from '../navbar/Navbar'
 
 interface BordLayoutProps {
   children: React.ReactNode
@@ -9,6 +10,7 @@ interface BordLayoutProps {
 export const BordLayout: React.FC<BordLayoutProps> = ({ children }) => (
   <div className="md-px-0 relative mx-auto mt-9 flex min-h-screen w-full bg-gray-100">
     <Sidebar />
+    <Navbar />
     {children}
   </div>
 )

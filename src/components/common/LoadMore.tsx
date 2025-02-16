8c166cbfedf2e48edaaa2cbb35f18e485358f532
Teerapong <teera04164@gmail.com>
2025-02-16 22:09:47 +0700

    add component load more

---
+++ src/components/common/LoadMore.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { RiLoader2Fill } from 'react-icons/ri';
+
+interface LoadMoreProps {
+  isFetchingMore: boolean;
+  loadMoreRef: (node?: Element | null) => void;
+  className?: string;
+}
+
+export const LoadMore: React.FC<LoadMoreProps> = ({
+  isFetchingMore,
+  loadMoreRef,
+  className = 'h-4'
+}) => {
+  return (
+    <div ref={loadMoreRef} className={className}>
+      {isFetchingMore && (
+        <div className="flex justify-center py-4">
+          <RiLoader2Fill className="w-6 h-6 animate-spin text-gray-500" />
+        </div>
+      )}
+    </div>
+  );
+};
